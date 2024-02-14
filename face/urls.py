498259a4a07d8553f2d3d34d5d3d8460f8979a6d
karan <patanikaran@gmail.com>
2024-02-14 16:30:19 +0530

    addd

--- face/urls.py
+++ face/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.face_re),
-    path('receive_image/', views.receive_image, name='recognize-face'),
-    path("p_img/",views.process_video,name="p_image")
+    path("",views.run, name='video_feed'),
+   # path('receive_image/', views.receive_image, name='recognize-face'),
+    #path("p_img/",views.process_video,name="p_image"),
+   # path('stop_video_feed/', views.stop_video_feed, name='stop_video_feed'),
+    #path('redirected_page/', views.redirected_page, name='redirected_page'),
+   path('video_feed/', views.video_feed, name='redirected_page'),
+
 ]
--- face/views.py
+++ face/views.py
@@ -1,107 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect,reverse
 import face_recognition
 import cv2
 import numpy as np
-import base64
-
-
-# Create your views here.
-# consumers.py
-import json
-from channels.generic.websocket import WebsocketConsumer
-
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
+from django.views.decorators.http import require_POST
 import face_recognition
 from django.http import JsonResponse
 
+
 def run(request):
+    
     return render(request,"p.html")
 
-# views.py
-from django.http import JsonResponse
+def gen_frames(cap):
+    while True:
+        obama_image = face_recognition.load_image_file("karanjecket.jpg")
+        obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-def receive_image(request):
-    if request.method == 'POST':
-        image_data = request.POST.get('image_data')
-        # Process and save image data as needed
-           # Remove the data URI scheme header
-        image_data_str = image_data.replace('data:image/jpeg;base64,', '')
-        
-        # Decode base64 string to bytes
-        image_data = base64.b64decode(image_data_str)
-        
-        # Convert bytes to numpy array
-        nparr = np.frombuffer(image_data, np.uint8)
-        
-        # Decode numpy array to image
-        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        video_capture =image
-        # You can then further process the image if needed
-        # For example, save it to a file
-        yash_image = face_recognition.load_image_file("yash.jpg")
-        yash_face_encoding = face_recognition.face_encodings(yash_image)[0]
-        known_face_encodings = [ yash_face_encoding]
-        known_face_names = [ "yash"]
-        face_locations = []
-        face_encodings = []
-        face_names = []
-        process_this_frame = True
-    
-        if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
-
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-            
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-
-                face_names.append(name)
-                print(face_names)
-
-       
-     
-        print("recived.........................................................")
-        return JsonResponse({'message': 'Image received successfully'})
-    return JsonResponse({'error': 'Invalid request method'}, status=400)
-
-
-def recognize_face(request):
-    if request.method == 'POST':
-        # Process the received image data for face recognition
-        image_data = request.POST.get('image_data')
-        print(image_data)
-        video_capture = cv2.VideoCapture(image_data)
+        # Load a second sample picture and learn how to recognize it.
+        biden_image = face_recognition.load_image_file("dharmik.jpg")
+        biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
         yash_image = face_recognition.load_image_file("yash.jpg")
         yash_face_encoding = face_recognition.face_encodings(yash_image)[0]
 
-     
+        swayam_image = face_recognition.load_image_file("swayam.jpg")
+        swayam_encoding = face_recognition.face_encodings(swayam_image)[0]
+        kenil_image = face_recognition.load_image_file("kenil.jpg")
+        kenil_encoding = face_recognition.face_encodings(kenil_image)[0]
 
         # Create arrays of known face encodings and their names
         known_face_encodings = [
-            yash_face_encoding
+            obama_face_encoding,
+            biden_face_encoding,
+            yash_face_encoding,swayam_encoding,kenil_encoding
         ]
         known_face_names = [
-            "yash"
+            "Karan",
+            "dharmik",
+            "yash","swayam","kenil"
         ]
 
         # Initialize some variables
@@ -109,12 +47,10 @@ def recognize_face(request):
         face_encodings = []
         face_names = []
         process_this_frame = True
-
-        while True:
-            # Grab a single frame of video
-            ret,frame = image_data.split
-
-            # Only process every other frame of video to save time
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
             if process_this_frame:
                 # Resize frame of video to 1/4 size for faster face recognition processing
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -166,133 +102,17 @@ def recognize_face(request):
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
             # Display the resulting image
-            cv2.imshow('Video', frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-            # Hit 'q' on the keyboard to quit!
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+def index(request):
+    return render(request, 'index.html')
+@gzip.gzip_page
+def video_feed(request):
+    cap= cv2.VideoCapture(0)
+    return StreamingHttpResponse(gen_frames(cap), content_type="multipart/x-mixed-replace;boundary=frame")
 
-        # Release handle to the webcam
-        video_capture.release()
-        # Perform face recognition using your face recognition model
-        # Replace the following with your face recognition logic
-        recognition_results = {'recognized_faces':face_names}
-        return JsonResponse(recognition_results)
-    else:
-        return JsonResponse({'error': 'Only POST requests are allowed'})
-
-
-def process_video(request):
-    if request.method == 'POST':
-        image_data_str = request.POST.get('image_data')
-        image_data_str = image_data_str.replace('data:image/jpeg;base64,', '')
-        image_data = base64.b64decode(image_data_str)
-        nparr = np.frombuffer(image_data, np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        
-        # Process the frame (e.g., detect faces)
-        # Example: Face detection using OpenCV's haarcascades
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
-        # Return the processed data (e.g., coordinates of detected faces)
-        return JsonResponse({'faces': faces.tolist()})
-    return JsonResponse({'error': 'Invalid request method'}, status=400)
-
-
-def face_re(request):
-    video_capture = cv2.VideoCapture(0)
-
-    # Load a sample picture and learn how to recognize it.
-    obama_image = face_recognition.load_image_file("yash.jpg")
-    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-    # Load a second sample picture and learn how to recognize it.
-  
-
-    # Create arrays of known face encodings and their names
-    known_face_encodings = [
-        obama_face_encoding,
-      
-    ]
-    known_face_names = [
-        "yash"
-      
-    ]
-
-    # Initialize some variables
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    process_this_frame = True
-
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-            
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
-
-# Run without arguments to find faces in live camera feed
 
     
-    return render(request,"p.html",{"face":face_names})
